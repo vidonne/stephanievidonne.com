f280a3faeb4fcc74e5389722cfab8e2a4cf2b1f2
Cedric Vidonne <cedric@vidonne.me>
2025-03-12 14:10:58 +0100

    add: hugo menu

--- themes/my-theme/assets/js/main.js
+++ themes/my-theme/assets/js/main.js
@@ -14,3 +14,24 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 //   .forEach(popover => {
 //     new bootstrap.Popover(popover)
 //   })
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  const navbarHeight = document.querySelector('.navbar').offsetHeight;
+  const links = document.querySelectorAll('a[href^="#"]');
+  for (const link of links) {
+    link.addEventListener("click", function(event) {
+      event.preventDefault();
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+      if (targetElement) {
+        const targetPosition = targetElement.offsetTop - navbarHeight;
+        window.scrollTo({
+          top: targetPosition,
+          behavior: "smooth"
+        });
+      }
+    });
+  }
+});
+
